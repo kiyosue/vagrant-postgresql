c9d3697964c8891c94fe7a7b3c39ce67607a976e
Sunao Kiyosue <kiyo@itm.ne.jp>
2014-03-22 10:00:29 +0900

    複数バージョン入るようにしたけど不安定　

--- vagrant_cookbooks/yum-pgdg/attributes/pgdg.rb
+++ vagrant_cookbooks/yum-pgdg/attributes/pgdg.rb
@@ -1,15 +1,50 @@
-default['yum']['pgdg']['version'] = '9.3'
-version = node['yum']['pgdg']['version']
+#version9.3
+default['yum']['pgdg93']['version'] = '9.3'
+version = node['yum']['pgdg93']['version']
 
-default['yum']['pgdg']['repositoryid'] = "pgdg-#{version}"
-default['yum']['pgdg']['description'] = "PostgreSQL #{version}"
-default['yum']['pgdg']['gpgkey'] = 'http://yum.postgresql.org/RPM-GPG-KEY-PGDG'
-default['yum']['pgdg']['gpgcheck'] = true
-default['yum']['pgdg']['enabled'] = true
+default['yum']['pgdg93']['repositoryid'] = "pgdg-#{version}"
+default['yum']['pgdg93']['description'] = "PostgreSQL #{version}"
+default['yum']['pgdg93']['gpgkey'] = 'http://yum.postgresql.org/RPM-GPG-KEY-PGDG'
+default['yum']['pgdg93']['gpgcheck'] = true
+default['yum']['pgdg93']['enabled'] = true
 
 case node['platform_family']
 when 'rhel'
-  default['yum']['pgdg']['baseurl'] = "http://yum.pgrpms.org/#{version}/redhat/rhel-$releasever-$basearch"
+  default['yum']['pgdg93']['baseurl'] = "http://yum.pgrpms.org/#{version}/redhat/rhel-$releasever-$basearch"
 when 'fedora'
-  default['yum']['pgdg']['baseurl'] = "http://yum.pgrpms.org/#{version}/fedora/fedora-$releasever-$basearch"
+  default['yum']['pgdg93']['baseurl'] = "http://yum.pgrpms.org/#{version}/fedora/fedora-$releasever-$basearch"
+end
+
+#version9.2
+default['yum']['pgdg92']['version'] = '9.2'
+version = node['yum']['pgdg92']['version']
+
+default['yum']['pgdg92']['repositoryid'] = "pgdg-#{version}"
+default['yum']['pgdg92']['description'] = "PostgreSQL #{version}"
+default['yum']['pgdg92']['gpgkey'] = 'http://yum.postgresql.org/RPM-GPG-KEY-PGDG'
+default['yum']['pgdg92']['gpgcheck'] = true
+default['yum']['pgdg92']['enabled'] = true
+
+case node['platform_family']
+when 'rhel'
+  default['yum']['pgdg92']['baseurl'] = "http://yum.pgrpms.org/#{version}/redhat/rhel-$releasever-$basearch"
+when 'fedora'
+  default['yum']['pgdg92']['baseurl'] = "http://yum.pgrpms.org/#{version}/fedora/fedora-$releasever-$basearch"
+end
+
+#version9.1
+default['yum']['pgdg91']['version'] = '9.1'
+version = node['yum']['pgdg91']['version']
+
+default['yum']['pgdg91']['repositoryid'] = "pgdg-#{version}"
+default['yum']['pgdg91']['description'] = "PostgreSQL #{version}"
+default['yum']['pgdg91']['gpgkey'] = 'http://yum.postgresql.org/RPM-GPG-KEY-PGDG'
+default['yum']['pgdg91']['gpgcheck'] = true
+default['yum']['pgdg91']['enabled'] = true
+
+case node['platform_family']
+when 'rhel'
+  default['yum']['pgdg91']['baseurl'] = "http://yum.pgrpms.org/#{version}/redhat/rhel-$releasever-$basearch"
+when 'fedora'
+  default['yum']['pgdg91']['baseurl'] = "http://yum.pgrpms.org/#{version}/fedora/fedora-$releasever-$basearch"
 end
--- vagrant_cookbooks/yum-pgdg/recipes/default.rb
+++ vagrant_cookbooks/yum-pgdg/recipes/default.rb
@@ -16,45 +16,155 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-yum_repository 'pgdg' do
-  description node['yum']['pgdg']['description']
-  baseurl node['yum']['pgdg']['baseurl']
-  mirrorlist node['yum']['pgdg']['mirrorlist']
-  gpgcheck node['yum']['pgdg']['gpgcheck']
-  gpgkey node['yum']['pgdg']['gpgkey']
-  enabled node['yum']['pgdg']['enabled']
-  cost node['yum']['pgdg']['cost']
-  exclude node['yum']['pgdg']['exclude']
-  enablegroups node['yum']['pgdg']['enablegroups']
-  failovermethod node['yum']['pgdg']['failovermethod']
-  http_caching node['yum']['pgdg']['http_caching']
-  include_config node['yum']['pgdg']['include_config']
-  includepkgs node['yum']['pgdg']['includepkgs']
-  keepalive node['yum']['pgdg']['keepalive']
-  max_retries node['yum']['pgdg']['max_retries']
-  metadata_expire node['yum']['pgdg']['metadata_expire']
-  mirror_expire node['yum']['pgdg']['mirror_expire']
-  priority node['yum']['pgdg']['priority']
-  proxy node['yum']['pgdg']['proxy']
-  proxy_username node['yum']['pgdg']['proxy_username']
-  proxy_password node['yum']['pgdg']['proxy_password']
-  repositoryid node['yum']['pgdg']['repositoryid']
-  sslcacert node['yum']['pgdg']['sslcacert']
-  sslclientcert node['yum']['pgdg']['sslclientcert']
-  sslclientkey node['yum']['pgdg']['sslclientkey']
-  sslverify node['yum']['pgdg']['sslverify']
-  timeout node['yum']['pgdg']['timeout']
+yum_repository 'pgdg93' do
+  description node['yum']['pgdg93']['description']
+  baseurl node['yum']['pgdg93']['baseurl']
+  mirrorlist node['yum']['pgdg93']['mirrorlist']
+  gpgcheck node['yum']['pgdg93']['gpgcheck']
+  gpgkey node['yum']['pgdg93']['gpgkey']
+  enabled node['yum']['pgdg93']['enabled']
+  cost node['yum']['pgdg93']['cost']
+  exclude node['yum']['pgdg93']['exclude']
+  enablegroups node['yum']['pgdg93']['enablegroups']
+  failovermethod node['yum']['pgdg93']['failovermethod']
+  http_caching node['yum']['pgdg93']['http_caching']
+  include_config node['yum']['pgdg93']['include_config']
+  includepkgs node['yum']['pgdg93']['includepkgs']
+  keepalive node['yum']['pgdg93']['keepalive']
+  max_retries node['yum']['pgdg93']['max_retries']
+  metadata_expire node['yum']['pgdg93']['metadata_expire']
+  mirror_expire node['yum']['pgdg93']['mirror_expire']
+  priority node['yum']['pgdg93']['priority']
+  proxy node['yum']['pgdg93']['proxy']
+  proxy_username node['yum']['pgdg93']['proxy_username']
+  proxy_password node['yum']['pgdg93']['proxy_password']
+  repositoryid node['yum']['pgdg93']['repositoryid']
+  sslcacert node['yum']['pgdg93']['sslcacert']
+  sslclientcert node['yum']['pgdg93']['sslclientcert']
+  sslclientkey node['yum']['pgdg93']['sslclientkey']
+  sslverify node['yum']['pgdg93']['sslverify']
+  timeout node['yum']['pgdg93']['timeout']
   action :create
 end
 
 package "postgresql93-server" do
   action :install
-  notifies :run, 'execute[postgres-initdb]'
+  notifies :run, 'execute[postgres93-initdb]'
 end
 
-execute 'postgres-initdb' do
+execute 'postgres93-initdb' do
   user "postgres"
   command "/usr/pgsql-9.3/bin/initdb -D /var/lib/pgsql/9.3/data --no-locale --encoding=UTF8"
   action :nothing
+end
+
+template "/var/lib/pgsql/9.3/data/postgresql.conf" do
+  source "postgresql93.conf.erb"
+  owner "postgres"
+  group "postgres"
+  mode 0600
+#  notifies :restart, 'service[postgresql-9.3]'
+end
+
+
+yum_repository 'pgdg92' do
+  description node['yum']['pgdg92']['description']
+  baseurl node['yum']['pgdg92']['baseurl']
+  mirrorlist node['yum']['pgdg92']['mirrorlist']
+  gpgcheck node['yum']['pgdg92']['gpgcheck']
+  gpgkey node['yum']['pgdg92']['gpgkey']
+  enabled node['yum']['pgdg92']['enabled']
+  cost node['yum']['pgdg92']['cost']
+  exclude node['yum']['pgdg92']['exclude']
+  enablegroups node['yum']['pgdg92']['enablegroups']
+  failovermethod node['yum']['pgdg92']['failovermethod']
+  http_caching node['yum']['pgdg92']['http_caching']
+  include_config node['yum']['pgdg92']['include_config']
+  includepkgs node['yum']['pgdg92']['includepkgs']
+  keepalive node['yum']['pgdg92']['keepalive']
+  max_retries node['yum']['pgdg92']['max_retries']
+  metadata_expire node['yum']['pgdg92']['metadata_expire']
+  mirror_expire node['yum']['pgdg92']['mirror_expire']
+  priority node['yum']['pgdg92']['priority']
+  proxy node['yum']['pgdg92']['proxy']
+  proxy_username node['yum']['pgdg92']['proxy_username']
+  proxy_password node['yum']['pgdg92']['proxy_password']
+  repositoryid node['yum']['pgdg92']['repositoryid']
+  sslcacert node['yum']['pgdg92']['sslcacert']
+  sslclientcert node['yum']['pgdg92']['sslclientcert']
+  sslclientkey node['yum']['pgdg92']['sslclientkey']
+  sslverify node['yum']['pgdg92']['sslverify']
+  timeout node['yum']['pgdg92']['timeout']
+  action :create
+end
+
+package "postgresql92-server" do
+  action :install
+  notifies :run, 'execute[postgres92-initdb]'
+end
+
+execute 'postgres92-initdb' do
+  user "postgres"
+  command "/usr/pgsql-9.2/bin/initdb -D /var/lib/pgsql/9.2/data --no-locale --encoding=UTF8"
+  action :nothing
+end
+
+template "/var/lib/pgsql/9.2/data/postgresql.conf" do
+  source "postgresql92.conf.erb"
+  owner "postgres"
+  group "postgres"
+  mode 0600
+#  notifies :restart, 'service[postgresql-9.2]'
+end
+
+
+yum_repository 'pgdg91' do
+  description node['yum']['pgdg91']['description']
+  baseurl node['yum']['pgdg91']['baseurl']
+  mirrorlist node['yum']['pgdg91']['mirrorlist']
+  gpgcheck node['yum']['pgdg91']['gpgcheck']
+  gpgkey node['yum']['pgdg91']['gpgkey']
+  enabled node['yum']['pgdg91']['enabled']
+  cost node['yum']['pgdg91']['cost']
+  exclude node['yum']['pgdg91']['exclude']
+  enablegroups node['yum']['pgdg91']['enablegroups']
+  failovermethod node['yum']['pgdg91']['failovermethod']
+  http_caching node['yum']['pgdg91']['http_caching']
+  include_config node['yum']['pgdg91']['include_config']
+  includepkgs node['yum']['pgdg91']['includepkgs']
+  keepalive node['yum']['pgdg91']['keepalive']
+  max_retries node['yum']['pgdg91']['max_retries']
+  metadata_expire node['yum']['pgdg91']['metadata_expire']
+  mirror_expire node['yum']['pgdg91']['mirror_expire']
+  priority node['yum']['pgdg91']['priority']
+  proxy node['yum']['pgdg91']['proxy']
+  proxy_username node['yum']['pgdg91']['proxy_username']
+  proxy_password node['yum']['pgdg91']['proxy_password']
+  repositoryid node['yum']['pgdg91']['repositoryid']
+  sslcacert node['yum']['pgdg91']['sslcacert']
+  sslclientcert node['yum']['pgdg91']['sslclientcert']
+  sslclientkey node['yum']['pgdg91']['sslclientkey']
+  sslverify node['yum']['pgdg91']['sslverify']
+  timeout node['yum']['pgdg91']['timeout']
+  action :create
+end
+
+package "postgresql91-server" do
+  action :install
+  notifies :run, 'execute[postgres91-initdb]'
+end
+
+execute 'postgres91-initdb' do
+  user "postgres"
+  command "/usr/pgsql-9.1/bin/initdb -D /var/lib/pgsql/9.1/data --no-locale --encoding=UTF8"
+  action :nothing
+end
+
+template "/var/lib/pgsql/9.1/data/postgresql.conf" do
+  source "postgresql91.conf.erb"
+  owner "postgres"
+  group "postgres"
+  mode 0600
+#  notifies :restart, 'service[postgresql-9.1]'
 end
 
